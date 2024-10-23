4e10eaa4dd4b5897ae1f2ae4d2c289f9dea6ff42
Mirkparla <MirkoParla@outlook.it>
2024-10-23 20:10:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
+  ['393201391411', '~𝕯𝖆𝖉𝖆💋', true],
 ] //Numero degli owner
 
 //────────────────────────────
